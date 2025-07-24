5aff7858b4064a2d0b48e0785dcaadf197164895
구하은 <haeuni@guha-eun-ui-MacBookPro.local>
2025-07-24 00:25:50 -0400

    Update Ui+Validation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,41 +1,26 @@
 package app;
 
-import client_service.DeepSeekClient.DeepSeekClient;
-import client_service.GoogleMapsClient.GoogleMapsClient;
-import client_service.Recommendation.RecommendationService;
-import entity.Location;
-import entity.Recommendation;
-import io.github.cdimascio.dotenv.Dotenv;
+import view.LoginView;
 
-import java.util.List;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
-        dsclient.extractKeywords();
 
-        // maps
-        Dotenv dotenv = Dotenv.load();
-        final int radiusInMeters = 5000;
-        final String postalCode = "M5S 2E4";
+        final JFrame application = new JFrame("Login");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        final GoogleMapsClient client = new GoogleMapsClient(radiusInMeters);
+        final CardLayout cardLayout = new CardLayout();
 
-        final List<Location> locations = client.serveLocations(postalCode);
-        if (locations == null || locations.isEmpty()) {
-            System.out.println("No locations found for the given postal code.");
-        }
-        else {
-            // RecommendationService to find the recommended locations
-            RecommendationService recommendationService = new RecommendationService(dsclient);
-            Recommendation recommendation = recommendationService.recommend(locations, 5); // Top 5
+        final JPanel views = new JPanel(cardLayout);
+        application.add(views);
 
-            System.out.println("Found " + recommendation.getLocations() + " locations:");
-            for (Location location : recommendation.getLocations()) {
-                System.out.println(location);
-            }
-        }
+        LoginView loginView = new LoginView();
+        views.add(loginView);
 
+        application.pack();
+        application.setVisible(true);
 
     }
 }
