322afe941521d7db44a7717470daa37dde5ab831
Isha Ruparelia <isha.ruparelia@mail.utoronto.ca>
2025-07-24 03:02:17 -0400

    commit w/ MapView

---
+++ src/main/java/view/MapView.java
@@ -0,0 +1,45 @@
+package view;
+
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import entity.Location;
+
+public class MapView extends JPanel {
+
+    public MapView(List<Location> locations) {
+        setLayout(new BorderLayout());
+
+        JXMapViewer mapViewer = new JXMapViewer();
+
+        OSMTileFactoryInfo tileFactoryInfo = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(tileFactoryInfo);
+        mapViewer.setTileFactory(tileFactory);
+
+        if (!locations.isEmpty()) {
+            Location first = locations.get(0);
+            GeoPosition center = new GeoPosition(first.getLatitude(),
+                    first.getLongitude());
+            mapViewer.setZoom(5);
+            mapViewer.setAddressLocation(center);
+        }
+
+        Set<Waypoint> waypoints = new HashSet<>();
+        for (Location loc : locations) {
+            waypoints.add(new DefaultWaypoint(new GeoPosition(loc.getLatitude(),
+                    loc.getLongitude())));
+        }
+
+        WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<>();
+        waypointPainter.setWaypoints(waypoints);
+
+        mapViewer.setOverlayPainter(waypointPainter);
+        add(mapViewer, BorderLayout.CENTER);
+    }
+}
