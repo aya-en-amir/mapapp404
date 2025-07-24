89b6b40afce5687be4cbc9c3aea58bdf692138a5
tasfia_ara <tasfia_ara@outlook.com>
2025-07-23 21:39:11 -0400

    committing Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,32 @@
 package app;
 
 import client_service.DeepSeekClient.DeepSeekClient;
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginViewModel;
+import use_case.login.LoginInputBoundary;
+import view.LoginView;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
-        dsclient.extractKeywords();
+        final JFrame application = new JFrame("Login");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        final CardLayout cardLayout = new CardLayout();
+
+        final JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        LoginView loginView = new LoginView();
+        views.add(loginView);
+
+        application.pack();
+        application.setVisible(true);
+
+//        Deepseek testing
+//        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
+//        dsclient.extractKeywords();
     }
 }
