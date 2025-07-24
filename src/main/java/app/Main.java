e8a1d4bfab76f145644afb41937635dee487f647
Haeungoo <43082157+Haeungoo@users.noreply.github.com>
2025-07-23 23:46:21 -0400

    Revert "Login page semi-working"

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,35 +1,6 @@
 package app;
 
 import client_service.DeepSeekClient.DeepSeekClient;
-tasfia.ara
-import interface_adapter.login.LoginController;
-import interface_adapter.login.LoginViewModel;
-import use_case.login.LoginInputBoundary;
-import view.LoginView;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class Main {
-    public static void main(String[] args) {
-        final JFrame application = new JFrame("Login");
-        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        final CardLayout cardLayout = new CardLayout();
-
-        final JPanel views = new JPanel(cardLayout);
-        application.add(views);
-
-        LoginView loginView = new LoginView();
-        views.add(loginView);
-
-        application.pack();
-        application.setVisible(true);
-
-//        Deepseek testing
-//        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
-//        dsclient.extractKeywords();
-
 import client_service.GoogleMapsClient.GoogleMapsClient;
 import entity.Location;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -60,6 +31,5 @@ public class Main {
         }
 
 
-main
     }
 }
