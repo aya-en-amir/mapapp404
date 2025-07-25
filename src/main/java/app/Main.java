3dbdea066f61b65fa54798adb57fbf494089190e
구하은 <haeuni@guha-eun-ui-MacBookPro.local>
2025-07-24 22:28:52 -0400

    Fix Main.java conflict

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -22,50 +22,5 @@ public class Main {
         application.pack();
         application.setVisible(true);
 
-//        Deepseek testing
-//        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
-//        dsclient.extractKeywords();
-
-import client_service.DeepSeekClient.DeepSeekClient;
-import client_service.GoogleMapsClient.GoogleMapsClient;
-import client_service.Recommendation.RecommendationService;
-import entity.Location;
-import entity.Recommendation;
-import io.github.cdimascio.dotenv.Dotenv;
-import view.RecommendationView;
-
-import java.util.Collections;
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        DeepSeekClient dsclient = new DeepSeekClient("I am stressed and anxious");
-        dsclient.extractKeywords();
-
-        // maps
-        Dotenv dotenv = Dotenv.load();
-        final int radiusInMeters = 5000;
-        final String postalCode = "M5S 2E4";
-
-        final GoogleMapsClient client = new GoogleMapsClient(radiusInMeters);
-        final List<Location> locations = client.serveLocations(postalCode);
-
-        List<Recommendation> recommendations;
-        if (locations == null || locations.isEmpty()) {
-            System.out.println("No locations found for the given postal code.");
-            return;
-        } else {
-            // RecommendationService to find the recommended locations
-            RecommendationService recommendationService = new RecommendationService(dsclient);
-            recommendations = Collections.singletonList(recommendationService.recommend(locations, 5));
-
-            for (Recommendation rec : recommendations) {
-                System.out.println(rec.getLocations());
-            }
-
-        }
-        new RecommendationView(recommendations);
-        main
-
     }
 }
