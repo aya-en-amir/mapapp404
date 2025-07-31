f613e3a258f248f670deeb78b552475874609633
boyleoli <olivia.boyle@mail.utoronto.ca>
2025-07-30 23:40:17 -0400

    cleaned up the code

--- src/main/java/client_service/ReviewMatcher/ReviewMatcher.java
+++ src/main/java/client_service/ReviewMatcher/ReviewMatcher.java
@@ -2,7 +2,11 @@ package client_service.ReviewMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.uniba.di.lacam.kdde.lexical_db.ILexicalDatabase;
 import edu.uniba.di.lacam.kdde.lexical_db.MITWordNet;
 import edu.uniba.di.lacam.kdde.ws4j.similarity.Path;
@@ -19,37 +23,11 @@ public class ReviewMatcher {
         this.location = location;
     }
 
-    public double calculateScoreWordnet() {
-        double totalScore = 0.0;
+    public String combineReview() {
 
-        if (location.getReviews().isEmpty()) {
-            return totalScore;
-        }
-        WS4JConfiguration.getInstance().setMemoryDB(false);
-        WS4JConfiguration.getInstance().setMFS(true);
-        ILexicalDatabase db = new MITWordNet();
-        WuPalmer wuPalmer = new WuPalmer(db);
-        for (String word : keywords) {
-            for (String locationReview : location.getReviews()) {
-                totalScore += wuPalmer.calcRelatednessOfWords(word.toLowerCase(), locationReview.toLowerCase());
-            }
-        }
-        return (totalScore / keywords.size()) * 100;
+        return String.join(" ", location.getReviews());
+
     }
-    public List<Double> calculateScoreTest() {
-        List<Double> scores = new ArrayList<Double>();
-        WS4JConfiguration.getInstance().setMemoryDB(false);
-        WS4JConfiguration.getInstance().setMFS(true);
-        ILexicalDatabase db = new MITWordNet();
-        WuPalmer wuPalmer = new WuPalmer(db);
-        Path path = new Path(db);
-        scores.add(wuPalmer.calcRelatednessOfWords("yummy", "delicious"));
-        scores.add(path.calcRelatednessOfWords("yummy", "delicious"));
-
-
-        return scores;
-    }
-
     public double calculateScore() {
         double totalScore = 0.0;
 
@@ -61,6 +39,26 @@ public class ReviewMatcher {
             for (String locationReview : location.getReviews()) {
                 CosineCalculator calculator = new CosineCalculator(word.toLowerCase(), locationReview.toLowerCase());
                 totalScore += calculator.calculate();
+            }
+        }
+        return (totalScore / keywords.size()) * 100;
+    }
+
+    public double calculateScoreWordnet() {
+        double totalScore = 0.0;
+
+        if (location.getReviews().isEmpty()) {
+            return totalScore;
+        }
+        List<String> lemma = StanfordCore.setStandfordCore(this.combineReview());
+
+        WS4JConfiguration.getInstance().setMemoryDB(false);
+        WS4JConfiguration.getInstance().setMFS(true);
+        ILexicalDatabase db = new MITWordNet();
+        WuPalmer wuPalmer = new WuPalmer(db);
+        for (String word : keywords) {
+            for (String review : lemma) {
+                totalScore += wuPalmer.calcRelatednessOfWords(word.toLowerCase(), review.toLowerCase());
             }
         }
         return (totalScore / keywords.size()) * 100;
--- src/test/java/ReviewMatcherTest.java
+++ src/test/java/ReviewMatcherTest.java
@@ -1,4 +1,9 @@
 import client_service.ReviewMatcher.ReviewMatcher;
+import edu.uniba.di.lacam.kdde.lexical_db.ILexicalDatabase;
+import edu.uniba.di.lacam.kdde.lexical_db.MITWordNet;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.Path;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.WuPalmer;
+import edu.uniba.di.lacam.kdde.ws4j.util.WS4JConfiguration;
 import org.junit.Test;
 
 import entity.Location;
@@ -10,29 +15,40 @@ import static org.junit.Assert.assertTrue;
 
 public class ReviewMatcherTest {
     @Test
-    public void successTest() {
+    public void testNumOne() {
         List<String> bahenReview = new ArrayList<>();
-        bahenReview.addAll(Arrays.asList("It's", "a", "tasty", "place", "The", "food", "needs", "more", "salt"));
+        bahenReview.addAll(Arrays.asList("It's", "very", "pale.", "The", "food", "needs", "more", "salt"));
         Location location = new Location("Bahen", 10.122f, 20.12f, bahenReview);
 
         List<String> keyword = new ArrayList<>();
         keyword.addAll(Arrays.asList("delicious", "yummy"));
         ReviewMatcher reviewMatcher = new ReviewMatcher(keyword, location);
-        double score = reviewMatcher.calculateScoreWordnet();
-        System.out.println(score);
+        System.out.println(reviewMatcher.calculateScoreWordnet());
         System.out.println(reviewMatcher.calculateScore());
     }
     @Test
-    public void getTest() {
+    public void testNumTwo() {
         List<String> bahenReview = new ArrayList<>();
         bahenReview.addAll(Arrays.asList("It's", "a", "depressing", "place"));
         Location location = new Location("Bahen", 10.122f, 20.12f, bahenReview);
 
         List<String> keyword = new ArrayList<>();
-        keyword.addAll(Arrays.asList("awesome", "great"));
+        keyword.addAll(Arrays.asList("awesome"));
         ReviewMatcher reviewMatcher = new ReviewMatcher(keyword, location);
-        List<Double> score = reviewMatcher.calculateScoreTest();
+        Double score = reviewMatcher.calculateScoreWordnet();
         System.out.println(score);
     }
+    @Test
+    public void testWordNet() {
+        WS4JConfiguration.getInstance().setMemoryDB(false);
+        WS4JConfiguration.getInstance().setMFS(true);
+        ILexicalDatabase db = new MITWordNet();
+        WuPalmer wuPalmer = new WuPalmer(db);
+
+        System.out.println(wuPalmer.calcRelatednessOfWords("water", "wet"));
+        System.out.println(wuPalmer.calcRelatednessOfWords("water", "drink"));
+        System.out.println(wuPalmer.calcRelatednessOfWords("tasty", "delicious"));
+    }
+
 }
 
