fbdff8a5de1ad370dfbf5c647001f8e0a7f82a2d
tasfia_ara <tasfia_ara@outlook.com>
2025-08-06 12:39:03 -0400

    MainTest adding (again)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import app.AppController;
+import client_service.api.GoogleMapsClient;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.Test;
+import view.LoginView;
+
+import javax.swing.*;
 
 public class MainTest {
+    @Test
+    public void loginViewTest(){
+        final JFrame application = new JFrame("Login");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setContentPane(new LoginView());
+        application.setSize(800, 400);
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
+    }
+
+    public void recommendationViewTest(){
+        final int radiusInMeters = 5000;
+        GoogleMapsClient gmaps = new GoogleMapsClient(radiusInMeters);
+        final String postalCode = "M5S2E4";
+
+    }
 }
